2b1807d77364ddff6aaf0c9f948d463a76a9a4c2
Lucas Impieri <lucasimpieri@gmail.com>
2020-11-03 15:07:54 -0300

    Projeto Criado

---
+++ src/test/java/com/impieri/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.impieri.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
