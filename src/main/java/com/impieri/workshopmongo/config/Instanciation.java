5ad5f7890c4efdc0151a70637fd6e410107d3153
Lucas Impieri <lucasimpieri@gmail.com>
2020-12-03 10:38:18 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/impieri/workshopmongo/config/Instanciation.java
+++ src/main/java/com/impieri/workshopmongo/config/Instanciation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.impieri.workshopmongo.domain.Post;
 import com.impieri.workshopmongo.domain.User;
 import com.impieri.workshopmongo.dto.AuthorDTO;
+import com.impieri.workshopmongo.dto.CommentDTO;
 import com.impieri.workshopmongo.repositories.PostRepository;
 import com.impieri.workshopmongo.repositories.UserRepository;
 
@@ -41,9 +42,17 @@ public class Instanciation implements CommandLineRunner{
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		
 		userRepository.save(maria);
 
 	}
--- src/main/java/com/impieri/workshopmongo/domain/Post.java
+++ src/main/java/com/impieri/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.impieri.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.impieri.workshopmongo.dto.AuthorDTO;
+import com.impieri.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -19,6 +22,8 @@ public class Post implements Serializable {
 	private String body;
 	private AuthorDTO author;
 
+	private List<CommentDTO> comments = new ArrayList<>();
+	
 	public Post() {
 	}
 
@@ -71,6 +76,15 @@ public class Post implements Serializable {
 		this.author = author;
 	}
 
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -95,4 +109,5 @@ public class Post implements Serializable {
 			return false;
 		return true;
 	}
+
 }
